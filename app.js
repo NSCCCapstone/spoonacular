e88da52051a15754226facc450e6888424001ea5
CBurton1 <chrislukeburton@gmail.com>
2017-03-24 11:10:30 -0300

    installed angular and added a .gitignore file

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.use(express.static('./public'));
 //fire controllers
 todoController(app);
 
+app.get('*', function(req, res) {
+    res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
 //listen to port
 app.listen(3000);
 console.log('You are listening to port 3000');
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,5 @@
 var bodyParser = require('body-parser');
+var unirest = require('unirest');
 
 var data = [{item: 'Item 1'}, {item: 'Item 2'}, {item: 'Item 3'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
@@ -21,5 +22,15 @@ module.exports = function(app){
         });
         res.json(data);
     });
+
+    var api_key = "Tvrn3k9JkhmshuZ8RNyT4i3v4G1Hp10U0YkjsnOd1d34u7DOaJ";
+
+    // These code snippets use an open-source library. http://unirest.io/nodejs
+    unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?addRecipeInformation=false&cuisine=american&excludeIngredients=coconut%2C+mango&fillIngredients=false&includeIngredients=onions%2C+lettuce%2C+tomato&instructionsRequired=false&intolerances=peanut%2C+shellfish&limitLicense=false&maxCalories=1500&maxCarbs=100&maxFat=100&maxProtein=100&minCalories=150&minCarbs=5&minFat=5&minProtein=5&number=10&offset=0&query=burger&ranking=1&type=main+course")
+        .header("X-Mashape-Key", "Tvrn3k9JkhmshuZ8RNyT4i3v4G1Hp10U0YkjsnOd1d34u7DOaJ")
+        .header("Accept", "application/json")
+        .end(function (result) {
+            console.log(result.status, result.headers, result.body);
+        });
 
 };
